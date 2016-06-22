b532f6231321c3f56a5288d1500ff564ea0f81ab
sharhar <wiishahar@gmail.com>
2016-06-22 15:45:42 -0700

    Fully documented Test class

--- PL-Test/src/net/poor/pleb/Test.java
+++ PL-Test/src/net/poor/pleb/Test.java
@@ -21,24 +21,24 @@ public class Test implements PLTcpClientCallback, PLTcpServerCallback{
 	PLTcpServer server;
 	
 	public Test() {
-		PLEngine.init();
+		PLEngine.init();//Initialize the engine
 		
 		server = new PLTcpServer(5000, this);//Create a tcp server on port 5000 with the 'this' object as a callback
 		client = new PLTcpClient("localhost", 5000, this);//Create a tcp client that connects to a server at "localhost" on port 5000 with the 'this' object as a callback
 		
-		server.start();
-		client.start();
+		server.start();//Start the server (SERVER MUST BE STARTED FIRST!)
+		client.start();//Start the client
 		
-		PLWindow window = new PLWindow(800, 600, "Engine");
-		PLGraphicsContext context = new PLGraphicsContextGL(true).create(window);
-		PLRenderer renderer = new PLRendererGL();
+		PLWindow window = new PLWindow(-1, -1, 800, 600, "Test", true);//Create a window with xOff:-1(centered), yOff:-1(centered), width:800 , height:800, title:"Test", and resizeable:true
+		PLGraphicsContext context = new PLGraphicsContextGL(true).create(window);//Create a an OpenGL Graphics interface
+		PLRenderer renderer = new PLRendererGL();//Create an OpenGL renderer
 		
-		while(window.isOpen()) {
-			renderer.clear(PLColor.MAGENTA);
-			PLEngine.pollEvents();
-			context.swapBuffers(window);
+		while(window.isOpen()) {//while the window is open
+			renderer.clear(PLColor.MAGENTA);//Clear the window and set the clear color to magenta
+			PLEngine.pollEvents();//Poll all events
+			context.swapBuffers(window);//Swap the graphics buffers
 			
-			client.sendData("PING");
+			client.sendData("PING");//Send the message "PING" to the server
 			
 			try {
 				Thread.sleep(100);//wait 100 milliseconds, so that the loop only runs about 10 times a second
@@ -47,7 +47,7 @@ public class Test implements PLTcpClientCallback, PLTcpServerCallback{
 			}
 		}
 		
-		PLEngine.terminate();
+		PLEngine.terminate();//Terminate everything
 	}
 	
 	public void receivedData(int id, String data) {//Server received data from client with ID 'id'
