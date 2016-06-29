69603aae08914c6e4d5b3af2f76277aba5d93105
sharhar <wiishahar@gmail.com>
2016-06-29 11:15:31 -0700

    Sound class

--- MC-Audio/SoundManager.h
+++ MC-Audio/SoundManager.h
@@ -1,4 +1,15 @@
 #pragma once
 
 #include <SDL/SDL_mixer.h>
+#include <AL/alc.h>
 
+namespace mc {
+	class SoundManager {
+	private:
+		static ALCdevice* m_device;
+		static ALCcontext* m_context;
+	public:
+		static void init();
+		static void destroy();
+	};
+}
