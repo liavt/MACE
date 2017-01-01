b6bfb71a4498d60bc7add0ff07996a1529b18a01
liavt <liavt@yahoo.com>
2017-01-01 12:30:44 -0600

    Working on running demos in tests

--- demos/Cleaning.cpp
+++ demos/Cleaning.cpp
@@ -1,102 +1,106 @@
+#define MACE_DEMO_NAME Cleaning
+
 #include <MACE/MACE.h>
 #include <random>
 #include <ctime>
 #include <iostream>
 
-using namespace mc;
+namespace {
+	using namespace mc;
 
-class FPSEntity: public gfx::Entity {
-	Index nbFrames;
-	time_t lastTime = time(0);
+	class FPSEntity: public gfx::Entity {
+		Index nbFrames;
+		time_t lastTime = time(0);
 
-	void onUpdate() override {};
-	void onRender() override {
-		nbFrames++;
-		if( time(0) - lastTime >= 1.0 ) {
-			std::cout << "FPS: " << nbFrames << " Frame Time: " << float(1000.0f) / nbFrames << std::endl;
-			nbFrames = 0;
-			lastTime += 1;
+		void onUpdate() override {};
+		void onRender() override {
+			nbFrames++;
+			if( time(0) - lastTime >= 1.0 ) {
+				std::cout << "FPS: " << nbFrames << " Frame Time: " << float(1000.0f) / nbFrames << std::endl;
+				nbFrames = 0;
+				lastTime += 1;
+			}
+		};
+		void onInit() override {};
+		void onDestroy() override {};
+
+	};
+
+	class TestComponent: public gfx::Component {
+
+		void init(gfx::Entity* en) override {
+			en->setProperty(gfx::Entity::STRETCH_X, true);
+			en->setProperty(gfx::Entity::STRETCH_Y, true);
+			dynamic_cast<gfx::GraphicsEntity*>(en)->setPaint(Colors::RED);
+		}
+
+		bool update(gfx::Entity* en) override {
+			if( en->getProperty(gfx::Entity::HOVERED) && os::Input::isKeyDown(os::Input::MOUSE_LEFT) ) {
+				en->makeDirty();
+			}
+			return false;
+		}
+
+		void destroy(gfx::Entity* en) override {}
+
+		void clean(gfx::Entity* en) override {
+			dynamic_cast<gfx::GraphicsEntity*>(en)->setPaint(Color((rand() % 10) / 10.0f, (rand() % 10) / 10.0f, (rand() % 10) / 10.0f, 0.5f));
 		}
 	};
-	void onInit() override {};
-	void onDestroy() override {};
 
-};
+	TestComponent component = TestComponent();
+	gfx::Group group = gfx::Group();
 
-class TestComponent: public gfx::Component {
-	
-	void init(gfx::Entity* en) override {
-		en->setProperty(gfx::Entity::STRETCH_X, true);
-		en->setProperty(gfx::Entity::STRETCH_Y, true);
-		dynamic_cast<gfx::GraphicsEntity*>(en)->setPaint(Colors::RED);
+	gfx::Image left;
+	gfx::Image leftBot;
+	gfx::Image rightTop;
+	gfx::Image rightBot;
+
+	void initGL() {
+		srand((unsigned) time(0));
+
+		const Size elementNum = 10;
+
+		left = gfx::Image(Colors::RED);
+		leftBot = gfx::Image(Colors::RED);
+		rightTop = gfx::Image(Colors::RED);
+		rightBot = gfx::Image(Colors::RED);
+
+
+		left.setY(0.0f);
+		left.setX(-0.5f);
+		left.setHeight(0.9f);
+		left.setWidth(0.45f);
+
+		leftBot.setX(0.0f);
+		leftBot.setY(-0.5f);
+		leftBot.setWidth(0.7f);
+		leftBot.setHeight(0.4f);
+
+		left.addChild(leftBot);
+
+		rightTop.setX(0.5f);
+		rightTop.setY(0.5f);
+		rightTop.setHeight(0.4f);
+		rightTop.setWidth(0.4f);
+
+		rightBot.setHeight(0.4f);
+		rightBot.setWidth(0.4f);
+		rightBot.setX(0.5f);
+		rightBot.setY(-0.5f);
+
+		left.addComponent(component);
+		leftBot.addComponent(component);
+		rightTop.addComponent(component);
+		rightBot.addComponent(component);
+
+		group.addChild(left);
+		group.addChild(rightTop);
+		group.addChild(rightBot);
 	}
+}//anon namespace
 
-	bool update(gfx::Entity* en) override {
-		if( en->getProperty(gfx::Entity::HOVERED) && os::Input::isKeyDown(os::Input::MOUSE_LEFT) ) {
-			en->makeDirty();
-		}
-		return false;
-	}
-	
-	void destroy(gfx::Entity* en) override {}
-
-	void clean(gfx::Entity* en) override {
-		dynamic_cast<gfx::GraphicsEntity*>(en)->setPaint(Color((rand() % 10) / 10.0f, (rand() % 10) / 10.0f, (rand() % 10) / 10.0f, 0.5f));
-	}
-};
-
-TestComponent component = TestComponent();
-gfx::Group group = gfx::Group();
-
-gfx::Image left;
-gfx::Image leftBot;
-gfx::Image rightTop;
-gfx::Image rightBot;
-
-void initGL() {
-	srand((unsigned) time(0));
-
-	const Size elementNum = 10;
-
-	left = gfx::Image(Colors::RED);
-	leftBot = gfx::Image(Colors::RED);
-	rightTop = gfx::Image(Colors::RED);
-	rightBot = gfx::Image(Colors::RED);
-
-
-	left.setY(0.0f);
-	left.setX(-0.5f);
-	left.setHeight(0.9f);
-	left.setWidth(0.45f);
-
-	leftBot.setX(0.0f);
-	leftBot.setY(-0.5f);
-	leftBot.setWidth(0.7f);
-	leftBot.setHeight(0.4f);
-
-	left.addChild(leftBot);
-
-	rightTop.setX(0.5f);
-	rightTop.setY(0.5f);
-	rightTop.setHeight(0.4f);
-	rightTop.setWidth(0.4f);
-
-	rightBot.setHeight(0.4f);
-	rightBot.setWidth(0.4f);
-	rightBot.setX(0.5f);
-	rightBot.setY(-0.5f);
-
-	left.addComponent(component);
-	leftBot.addComponent(component);
-	rightTop.addComponent(component);
-	rightBot.addComponent(component);
-
-	group.addChild(left);
-	group.addChild(rightTop);
-	group.addChild(rightBot);
-}
-
-int main(int argc, char** argv) {
+int MACE_DEMO_MAIN() {
 	try {
 
 		os::WindowModule module = os::WindowModule(600, 500, "Cleaning Demo");
@@ -120,7 +124,11 @@ int main(int argc, char** argv) {
 		while( mc::System::isRunning() ) {
 			mc::System::update();
 
+#ifdef MACE_TEST_DEMOS
+			mc::System::requestStop();
+#else
 			std::this_thread::sleep_for(std::chrono::milliseconds(33));
+#endif		
 		}
 
 		mc::System::destroy();
--- demos/Rotations.cpp
+++ demos/Rotations.cpp
@@ -1,113 +1,117 @@
+#define MACE_DEMO_NAME Rotations
+
 #include <MACE/MACE.h>
 #include <random>
 #include <ctime>
 #include <iostream>
 
-using namespace mc;
+namespace {
+	using namespace mc;
 
-gfx::ogl::Texture star = gfx::ogl::Texture();
+	gfx::ogl::Texture star = gfx::ogl::Texture();
 
-bool rotating = false;
+	bool rotating = false;
 
-gfx::Group botLeft, botRight, topLeft, topRight;
+	gfx::Group botLeft, botRight, topLeft, topRight;
 
-class FPSEntity: public gfx::Entity {
-	Index nbFrames;
-	time_t lastTime = time(0);
+	class FPSEntity: public gfx::Entity {
+		Index nbFrames;
+		time_t lastTime = time(0);
 
-	void onUpdate() override {};
-	void onRender() override {
-		nbFrames++;
-		if( time(0) - lastTime >= 1.0 ) {
-			std::cout << "FPS: " << nbFrames << " Frame Time: " << float(1000.0f) / nbFrames << std::endl;
-			nbFrames = 0;
-			lastTime += 1;
-		}
+		void onUpdate() override {};
+		void onRender() override {
+			nbFrames++;
+			if( time(0) - lastTime >= 1.0 ) {
+				std::cout << "FPS: " << nbFrames << " Frame Time: " << float(1000.0f) / nbFrames << std::endl;
+				nbFrames = 0;
+				lastTime += 1;
+			}
+		};
+		void onInit() override {};
+		void onDestroy() override {};
 	};
-	void onInit() override {};
-	void onDestroy() override {};
-};
 
-class TestComponent: public gfx::Component {
+	class TestComponent: public gfx::Component {
 
-	void init(gfx::Entity* en) override {
-		dynamic_cast<gfx::Image*>(en)->setPaint(Color((rand() % 10) / 10.0f, (rand() % 10) / 10.0f, (rand() % 10) / 10.0f, 0.5f));
-	}
-
-	bool update(gfx::Entity* en) override {
-		if( en->getProperty(gfx::Entity::HOVERED) && os::Input::isKeyDown(os::Input::MOUSE_LEFT) ) {
-			en->rotate(0.0f, 0.0f, 0.01f);
+		void init(gfx::Entity* en) override {
+			dynamic_cast<gfx::Image*>(en)->setPaint(Color((rand() % 10) / 10.0f, (rand() % 10) / 10.0f, (rand() % 10) / 10.0f, 0.5f));
 		}
-		return false;
-	}
 
-	void destroy(gfx::Entity* en) override {}
-
-	void clean(gfx::Entity* en) override {}
-};
-
-class RotationComponent: public gfx::Component {
-	void init(gfx::Entity* e) {};
-	bool update(gfx::Entity* e) {
-		if( rotating ) {
-			e->rotate(0.0f, 0.0f, 0.01f);
+		bool update(gfx::Entity* en) override {
+			if( en->getProperty(gfx::Entity::HOVERED) && os::Input::isKeyDown(os::Input::MOUSE_LEFT) ) {
+				en->rotate(0.0f, 0.0f, 0.01f);
+			}
+			return false;
 		}
-		return false;
+
+		void destroy(gfx::Entity* en) override {}
+
+		void clean(gfx::Entity* en) override {}
 	};
-	void destroy(gfx::Entity* e) {};
-};
 
-RotationComponent r = RotationComponent();
-TestComponent testComponent = TestComponent();
+	class RotationComponent: public gfx::Component {
+		void init(gfx::Entity* e) {};
+		bool update(gfx::Entity* e) {
+			if( rotating ) {
+				e->rotate(0.0f, 0.0f, 0.01f);
+			}
+			return false;
+		};
+		void destroy(gfx::Entity* e) {};
+	};
+
+	RotationComponent r = RotationComponent();
+	TestComponent testComponent = TestComponent();
 
 
-void initGL() {
+	void initGL() {
 
 
 
-	srand((unsigned) time(0));
+		srand((unsigned) time(0));
 
-	const Size elementNum = 10;
+		const Size elementNum = 10;
 
-	star = gfx::ogl::Texture(MACE_DEMO_ASSETS + std::string("star.png"));
+		star = gfx::ogl::Texture(MACE_DEMO_ASSETS + std::string("star.png"));
 
-	for( Index x = 0; x < elementNum; x++ ) {
-		for( Index y = 0; y < elementNum; y++ ) {
-			gfx::Image* entity = new gfx::Image();
+		for( Index x = 0; x < elementNum; x++ ) {
+			for( Index y = 0; y < elementNum; y++ ) {
+				gfx::Image* entity = new gfx::Image();
 
-			entity->setTexture(star);
+				entity->setTexture(star);
 
-			entity->setX((((x % (elementNum / 2)) *(1.0f / elementNum)) * 4) - (1.0f - (1.0f / elementNum) * 2));
-			entity->setY((((y % (elementNum / 2)) * (1.0f / elementNum)) * 4) - (1.0f - (1.0f / elementNum) * 2));
+				entity->setX((((x % (elementNum / 2)) *(1.0f / elementNum)) * 4) - (1.0f - (1.0f / elementNum) * 2));
+				entity->setY((((y % (elementNum / 2)) * (1.0f / elementNum)) * 4) - (1.0f - (1.0f / elementNum) * 2));
 
-			entity->setWidth((1.0f / elementNum) * 2);
-			entity->setHeight((1.0f / elementNum) * 2);
+				entity->setWidth((1.0f / elementNum) * 2);
+				entity->setHeight((1.0f / elementNum) * 2);
 
-			entity->setProperty(gfx::Entity::STRETCH_X, false);
-			entity->setProperty(gfx::Entity::STRETCH_Y, false);
+				entity->setProperty(gfx::Entity::STRETCH_X, false);
+				entity->setProperty(gfx::Entity::STRETCH_Y, false);
 
-			entity->addComponent(testComponent);
+				entity->addComponent(testComponent);
 
-			if( x >= elementNum / 2 ) {
-				if( y >= elementNum / 2 ) {
-					topRight.addChild(*entity);
+				if( x >= elementNum / 2 ) {
+					if( y >= elementNum / 2 ) {
+						topRight.addChild(*entity);
+					} else {
+						botRight.addChild(*entity);
+					}
 				} else {
-					botRight.addChild(*entity);
-				}
-			} else {
-				if( y >= elementNum / 2 ) {
-					topLeft.addChild(*entity);
-				} else {
-					botLeft.addChild(*entity);
+					if( y >= elementNum / 2 ) {
+						topLeft.addChild(*entity);
+					} else {
+						botLeft.addChild(*entity);
+					}
 				}
 			}
 		}
+
+
 	}
+}//anon namespace
 
-
-}
-
-int main(int argc, char** argv) {
+int MACE_DEMO_MAIN() {
 	try {
 		std::cout << "Click on a star to rotate it individually! It is pixel perfect! Holding down space will rotate all the stars in big chunks. It will only swap frames when something changes for maximum performance.\n";
 		std::cout << "Also try resizing the screen and watch how it reacts!\n";
@@ -168,14 +172,14 @@ int main(int argc, char** argv) {
 			} else {
 				rotating = false;
 			}
-
+#ifdef MACE_TEST_DEMOS
+			mc::System::requestStop();
+#else
 			std::this_thread::sleep_for(std::chrono::milliseconds(33));
+#endif
 		}
 
 		mc::System::destroy();
-
-
-
 	} catch( const std::exception& e ) {
 		Exception::handleException(e);
 		return -1;
