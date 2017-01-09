580b885b5a4385b982a805c5c1cc4e0524d1cab6
liavt <liavt@yahoo.com>
2017-01-08 21:56:34 -0600

    Fixed on non-windows systems

--- src/Graphics/Entity2D.cpp
+++ src/Graphics/Entity2D.cpp
@@ -8,6 +8,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 #include <MACE/Graphics/Entity2D.h>
+#include <cmath>
 
 namespace mc {
 	namespace gfx {
