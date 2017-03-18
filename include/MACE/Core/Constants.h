07f5e88f3d86618f7cb3ec2ce4a8c72c262b4f98
liavt <liavt@yahoo.com>
2017-03-17 19:40:52 -0500

    Removed pause in testing

--- include/MACE/Core/Constants.h
+++ include/MACE/Core/Constants.h
@@ -42,7 +42,7 @@ The above copyright notice and this permission notice shall be included in all c
 #		define MACE_FUNCTION_EXPORT __declspec(dllexport)
 #		define MACE_FUNCTION_IMPORT __declspec(dllimport)
 #	endif
-#elif defined(MACE_GNU) && __GNUC__ >= 4
+#elif defined(MACE_CLANG)||(defined(MACE_GNU) && __GNUC__ >= 4)
 #	define MACE_FUNCTION_EXPORT __attribute__((visibility("default")))
 #	define MACE_FUNCTION_IMPORT __attribute__((visibility("hidden")))
 #else
