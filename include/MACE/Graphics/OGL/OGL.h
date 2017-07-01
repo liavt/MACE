ee1489cca5df763344e44ebdf5a8d67f7da71f52
liavt <liavt@yahoo.com>
2017-07-01 21:09:03 +0300

    added override tofunction

--- include/MACE/Graphics/OGL/OGL.h
+++ include/MACE/Graphics/OGL/OGL.h
@@ -443,7 +443,7 @@ namespace mc {
 
 				void getImage(const Texture::Format format, const Texture::Type type, void* data) const override;
 
-				void setSwizzle(const Texture::SwizzleMode mode, const Texture::SwizzleMode arg);
+				void setSwizzle(const Texture::SwizzleMode mode, const Texture::SwizzleMode arg) override;
 
 				/**
 				@copydoc Object::operator==(const Object&) const
