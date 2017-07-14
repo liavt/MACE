438db3a4d4148db72280d566e4e78ca522ff8d91
liavt <liavt@yahoo.com>
2017-07-14 21:50:21 +0300

    working on automatic deployment

--- scripts/travis-before-deploy.sh
+++ scripts/travis-before-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd build
-echo "Generating x32 release..."
+echo "Generating static build release..."
 cmake -DMACE_TESTS=OFF -DMACE_DEMOS=OFF -DCMAKE_BUILD_TYPE=Release -DMACE_ALL_WARNINGS=OFF ..
 make MACE -j4
 ls
@@ -9,13 +9,12 @@ cd ..
 mkdir release
 mkdir release/static
 cp build/libMACE.a release/static/libMACE.a
-cp -a build/lib/. release/static/lib/
 cp -a build/include/. release/static/include/
 cp -a include/. release/static/include/
 rm -rf build
 mkdir build
 cd build
-echo "Generating x64 release..."
+echo "Generating shared build release..."
 cmake -DMACE_TESTS=OFF -DMACE_DEMOS=OFF -DCMAKE_BUILD_TYPE=Release -DMACE_ALL_WARNINGS=OFF -DBUILD_SHARED_LIBS=ON ..
 make MACE -j4
 ls
@@ -23,7 +22,6 @@ cd ..
 mkdir release/shared
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then cp build/libMACE.dylib release/shared/libMACE.dylib; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then cp build/libMACE.so release/shared/libMACE.so; fi
-cp -a build/lib/. release/shared/lib/
 cp -a build/include/. release/shared/include/
 cp -a include/. release/shared/include/
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then tar -zcf osx-x64.tar.gz release; fi
