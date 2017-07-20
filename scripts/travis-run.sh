b2db912fe6019e023d4931487d0a04fe72c98a3a
liavt <liavt@yahoo.com>
2017-07-20 16:03:45 +0300

    fixed travis deployment script

---
+++ scripts/travis-run.sh
@@ -0,0 +1,17 @@
+make all -j4;
+ls;
+./MACE-Testing;
+cd ..;
+
+if [[ $TRAVIS_OS_NAME == osx || $COMPILER == g++-6 ]]; then
+	git config --global user.email "travis@travis-ci.org";
+	git config --global user.name "Travis CI";
+
+	TAG_NAME = "cat VERSION";
+	if[[ $COMPILER == g++-6 ]]; then
+		git tag -a $TAG_NAME -m "Version $TAG_NAME";
+	else
+		git tag $TAG_NAME;
+	fi
+	git push https://${GITHUB_TOKEN}@github.com/liavt/MACE.git --tags;
+fi
