1f105182491723b393e2cd7baef89e6f46f22041
liavt <liavt@yahoo.com>
2017-07-20 18:55:32 +0300

    attempt at fixing weird working directory bug

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+cd build;
 make all -j4;
 ls;
 ./MACE-Testing;
-cd ..;
+cd ..
 
 if [[ $TRAVIS_OS_NAME == osx || $COMPILER == g++-6 ]]; then
 	git config --global user.email "travis@travis-ci.org";
 	git config --global user.name "Travis CI";
-
+	
+	git fetch --tags;
 	TAG_NAME=$(<VERSION);
 	git tag ${TAG_NAME};
 	git push https://${GITHUB_TOKEN}@github.com/liavt/MACE.git --tags;
